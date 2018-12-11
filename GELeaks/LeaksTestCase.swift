44532846d7124f78bd6db155fd801256a55f9afb
Grigory Entin <grigory.entin@gmail.com>
2018-12-11 21:47:33 +0100

    Segregated testLeaks(for: XCTestCase.Type[, method:])/allClasses/allMethods(for: AnyClass).

--- GELeaks/LeaksTestCase.swift
+++ GELeaks/LeaksTestCase.swift
@@ -41,74 +41,88 @@ extension XCTestCase {
 }
 
 class LeaksTestCase: XCTestCase {
-	
-	func testLeaks() {
-		var numClasses: Int32 = 0
-		var allClasses: AutoreleasingUnsafeMutablePointer<AnyClass>? = nil
-		defer {
-			allClasses = nil
-		}
-		
-		numClasses = objc_getClassList(nil, 0)
-		
-		if numClasses > 0 {
-			var ptr = UnsafeMutablePointer<AnyClass>.allocate(capacity: Int(numClasses))
-			defer {
-				ptr.deinitialize(count: Int(numClasses))
-			}
-			allClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(ptr)
-			numClasses = objc_getClassList(allClasses, numClasses)
-			
-			for i in 0 ..< numClasses {
-				guard let cls: AnyClass = allClasses?[Int(i)] else {
-					continue
-				}
-				guard Bundle(for: cls) === Bundle(for: type(of: self)) else {
-					continue
-				}
-				guard let testCaseClass = cls as? XCTestCase.Type else {
-					continue
-				}
-				guard cls != type(of: self) else {
-					continue
-				}
-				
-				var numMethods: UInt32 = 0
-				guard let allMethods = class_copyMethodList(cls, &numMethods) else {
-					assertionFailure()
-					continue
-				}
+    
+    func testLeaks() {
+        let classes = allClasses()
+        for cls in classes {
+            testLeaks(for: cls)
+        }
+    }
 
-				for j in 0..<Int(numMethods) {
-					let method = allMethods[j]
-					let selector = method_getName(method)
-					let methodName = NSStringFromSelector(selector)
-					guard methodName.hasPrefix("test"), !methodName.hasSuffix(":") else {
-						continue
-					}
+    lazy var testBundle = Bundle(for: type(of: self))
 
-                    let reportLeak = { (i: FBAllocationTrackerSummary, randomCount: Int) in
-                        let leakedCls: AnyClass = NSClassFromString(i.className)!
-                        if i.aliveObjects % randomCount == 0 {
-                            let times = i.aliveObjects / randomCount
-                            print("\(cls).\(methodName): \(leakedCls) is likely leaked \(times) times.")
-                        } else {
-                            print("\(cls).\(methodName): \(leakedCls) is potentially leaked.")
-                        }
-                    }
-                    let exception = NSException.catch({
-                        self.testLeaks(reportLeak: reportLeak) {
-                            autoreleasepool {
-                                let c = testCaseClass.init(selector: selector)
-                                c.invokeTest()
-                            }
-                        }
-                    })
-                    if let exception = exception {
-                        print("\(cls).\(methodName): Caught \(exception).")
-                    }
-				}
-			}
-		}
-	}
+    func testLeaks(for cls: AnyClass) {
+        guard Bundle(for: cls) === testBundle else {
+            return
+        }
+        guard let testCaseClass = cls as? XCTestCase.Type else {
+            return
+        }
+        guard testCaseClass != type(of: self) else {
+            return
+        }
+        
+        let methods = allMethods(for: cls)
+        for method in methods {
+            testLeaks(for: testCaseClass, method: method)
+        }
+    }
+    
+    func testLeaks(for cls: XCTestCase.Type, method: Method) {
+        let selector = method_getName(method)
+        let methodName = NSStringFromSelector(selector)
+        guard methodName.hasPrefix("test"), !methodName.hasSuffix(":") else {
+            return
+        }
+        let reportLeak = { (i: FBAllocationTrackerSummary, randomCount: Int) in
+            let leakedCls: AnyClass = NSClassFromString(i.className)!
+            if i.aliveObjects % randomCount == 0 {
+                let times = i.aliveObjects / randomCount
+                print("\(cls).\(methodName): \(leakedCls) is likely leaked \(times) times.")
+            } else {
+                print("\(cls).\(methodName): \(leakedCls) is potentially leaked.")
+            }
+        }
+        let exception = NSException.catch({
+            self.testLeaks(reportLeak: reportLeak) {
+                autoreleasepool {
+                    let c = cls.init(selector: selector)
+                    c.invokeTest()
+                }
+            }
+        })
+        if let exception = exception {
+            print("\(cls).\(methodName): Caught \(exception).")
+        }
+    }
+}
+
+func allClasses() -> [AnyClass] {
+    let estimatedNumClasses = objc_getClassList(nil, 0)
+    
+    guard estimatedNumClasses > 0 else {
+        return []
+    }
+    
+    let allClasses = UnsafeMutablePointer<AnyClass>.allocate(capacity: Int(estimatedNumClasses))
+    defer {
+        allClasses.deinitialize(count: Int(estimatedNumClasses))
+        allClasses.deallocate()
+    }
+    
+    let numClasses = objc_getClassList(AutoreleasingUnsafeMutablePointer(allClasses), estimatedNumClasses)
+    
+    return Array(UnsafeBufferPointer(start: allClasses, count: Int(numClasses)))
+}
+
+func allMethods(for cls: AnyClass) -> [Method] {
+    var numMethods: UInt32 = 0
+    guard let allMethods = class_copyMethodList(cls, &numMethods) else {
+        assertionFailure()
+        return []
+    }
+    defer {
+        free(allMethods)
+    }
+    return Array(UnsafeBufferPointer(start: allMethods, count: Int(numMethods)))
 }
