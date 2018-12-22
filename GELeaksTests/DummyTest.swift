d8cd53b6241023a09584fe6522de4e52640c6c1e
Grigory Entin <grigory.entin@gmail.com>
2018-12-22 05:08:04 +0100

    Added DummyTest.

--- GELeaksTests/DummyTest.swift
+++ GELeaksTests/DummyTest.swift
@@ -1,6 +1,6 @@
 //
 //  DummyTest.swift
-//  GELeaks-iOS-Unit-Tests
+//  GELeaks
 //
 //  Created by Grigory Entin on 20/12/2018.
 //
@@ -8,4 +8,14 @@
 import XCTest
 
 class DummyTest : XCTestCase {
+	
+	func testDummy() {
+		let a = NSMutableArray()
+		a.add(DummyObject())
+		#if false
+		a.add(a)
+		#endif
+	}
 }
+
+class DummyObject : NSObject {}
