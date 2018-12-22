974a98a69ca1f4dc07f8646d1e45e2c4da515c8c
Grigory Entin <grigory.entin@gmail.com>
2018-12-22 06:40:48 +0100

    Segregated LeaksDetectionConfig; added option for excluding system classes.

--- GELeaks/XCTestCase+Leaks.swift
+++ GELeaks/XCTestCase+Leaks.swift
@@ -8,30 +8,6 @@
 import FBAllocationTracker
 import XCTest
 
-struct LeakDetectionConfig {
-	let preheatCount: Int
-	let randomCount: Int
-	
-	typealias ReportLeak = (_ testCaseClass: AnyClass, _ selector: Selector, _ allocationSummary: FBAllocationTrackerSummary, _ config: LeakDetectionConfig) -> Void
-	
-	let reportLeak: ReportLeak
-	
-	init(preheatCount: Int = 2, randomCount: Int = 3, reportLeak: @escaping ReportLeak = defaultReportLeak) {
-		self.preheatCount = preheatCount
-		self.randomCount = randomCount
-		self.reportLeak = reportLeak
-	}
-}
-
-func defaultReportLeak(_ testCaseClass: AnyClass, _ selector: Selector, _ allocationSummary: FBAllocationTrackerSummary, _ config: LeakDetectionConfig) {
-	let leakedCls: AnyClass = NSClassFromString(allocationSummary.className)!
-	if allocationSummary.aliveObjects % config.randomCount == 0 {
-		let times = allocationSummary.aliveObjects / config.randomCount
-		XCTFail("\(testCaseClass).\(selector): \(leakedCls) is likely leaked (or left retained after the completion) \(times) times.")
-	} else {
-		print("\(testCaseClass).\(selector): \(leakedCls) is potentially leaked.")
-	}
-}
 
 extension XCTestCase {
 	
@@ -69,14 +45,14 @@ extension XCTestCase {
 		let allocationSummary = allocationTrackerManager.currentAllocationSummary()
 		allocationTrackerManager.stopTrackingAllocations()
 		for i in allocationSummary ?? [] where i.aliveObjects >= 0 {
-			let cls: AnyClass = NSClassFromString(i.className)!
-			#if false
-			let bundle = Bundle(for: cls)
-			let bundlePath = bundle.bundlePath
-			if bundlePath.hasPrefix("/System/") || bundlePath.hasPrefix("/Applications/") || bundlePath.hasPrefix("/Library/"){
-				continue
+			if config.excludeSystemClasses {
+				let cls: AnyClass = NSClassFromString(i.className)!
+				let bundle = Bundle(for: cls)
+				let bundlePath = bundle.bundlePath
+				if bundlePath.hasPrefix("/System/") || bundlePath.hasPrefix("/Applications/") || bundlePath.hasPrefix("/Library/"){
+					continue
+				}
 			}
-			#endif
 			config.reportLeak(self, selector, i, config)
 		}
 	}
