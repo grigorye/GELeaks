895a02aa9d3a6236dfae0f95c3449c49345494fb
Grigory Entin <grigory.entin@gmail.com>
2018-12-29 12:13:43 +0100

    Removed testXXX filter in favour of custom selectors (as e.g. in Quick).

--- GELeaks/Detection/XCTestCase+Leaks.swift
+++ GELeaks/Detection/XCTestCase+Leaks.swift
@@ -14,9 +14,6 @@ extension XCTestCase {
 	class func testLeaks(for method: Method, config: LeakDetectionConfig, leaksTestCase: XCTestCase) {
 		let selector = method_getName(method)
 		let methodName = NSStringFromSelector(selector)
-		guard methodName.hasPrefix("test"), !methodName.hasSuffix(":") else {
-			return
-		}
 		let cls = self
 		let exception = NSException.catch({
 			self.testLeaks(selector: selector, config: config, leaksTestCase: leaksTestCase)
