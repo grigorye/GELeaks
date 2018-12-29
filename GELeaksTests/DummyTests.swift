fa33fad8a9cd962932572b30dda10ec4329c2cc7
Grigory Entin <grigory.entin@gmail.com>
2018-12-29 10:18:05 +0100

    Segregated leakAsNecessary.

--- GELeaksTests/DummyTests.swift
+++ GELeaksTests/DummyTests.swift
@@ -12,16 +12,12 @@ class DummyTests : XCTestCase {
 	private let dummyProperty = DummyProperty()
 	
 	func testDummy() {
-		let a = NSMutableArray()
-		a.add(DummyObject())
-		#if false
-		a.add(a)
-		#endif
+		leakAsNecessary()
 	}
 	
 	func testMoreDummy() {
 	}
-
+	
 	/// When enabled, triggers a failure that is expected to be handled without a problem.
 	func notestWaiter() {
 		let expectation = self.expectation(description: "foo")
@@ -55,4 +51,12 @@ class DummyProperty : NSObject {
 	}
 	
 	deinit {()}
+}
+
+func leakAsNecessary() {
+	let a = NSMutableArray()
+	a.add(DummyObject())
+	#if false
+	a.add(a)
+	#endif
 }
